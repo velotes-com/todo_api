3b6977a9634343858d2c5eed9a16c01b63f0d02f
Vladimir <velotes@ya.ru>
2025-07-20 23:40:17 +0300

    DATABASES update for PostgreSQL

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'todo_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',
+        'USER': '',
+        'PASSWORD':'kokos',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
