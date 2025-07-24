fee3d6ccdc283b26aa499f0960ad6dd9650a3319
Vladimir <velotes@ya.ru>
2025-07-24 12:46:53 +0300

    update models.py & admin.py

--- todo_api/api/admin.py
+++ todo_api/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Task, Category, Priority
+
+
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(Category)
+admin.site.register(Priority)
+
+
--- todo_api/api/models.py
+++ todo_api/api/models.py
@@ -1,5 +1,3 @@
-# from queue import PriorityQueue
-
 from django.db import models
 from django.contrib.auth.models import User
 
