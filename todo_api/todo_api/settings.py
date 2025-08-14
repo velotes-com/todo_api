d7b5223460be751050d96f30d9ae5ad49d2c4e40
Vladimir <153288910+velotes-com@users.noreply.github.com>
2025-08-14 12:40:56 +0300

    Update settings.py

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "api.apps.ApiConfig",
+    "rest_framework.authtoken",
+    "api",
+    # "api.apps.ApiConfig",
+    "django_filters",
+    "todo_api"
 
 ]
 
@@ -85,6 +89,18 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter"],
 }
 
 
