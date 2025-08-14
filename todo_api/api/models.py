a4fabfeb5503769dd4e91009d26642cf4779391b
Vladimir <153288910+velotes-com@users.noreply.github.com>
2025-08-14 12:37:32 +0300

    Update models.py
    update

--- todo_api/api/models.py
+++ todo_api/api/models.py
@@ -1,23 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
 class Task(models.Model):
-    create_by = models.ForeignKey(User, on_delete=models.CASCADE),
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     status = models.CharField(max_length=25, choices=[
         ("new", "New"),
         ("in_progress", "In Progress"),
         ("completed", "Completed"),
-    ]  , default='new')
+    ], default='new')
     completed = models.BooleanField(default=False)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     completed_at = models.DateTimeField(null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
     deleted = models.BooleanField(default=False)
     category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True, blank=True)
     priority = models.ForeignKey("Priority", on_delete=models.SET_NULL, null=True, blank=True)
+
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
 
     def __str__(self):
         return self.title
@@ -30,6 +38,12 @@ class Category(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True)
     deleted = models.BooleanField(default=False)
 
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    class Meta:
+        unique_together = ("created_at", "name")
+
     def __str__(self):
         return self.name
 
@@ -39,6 +53,12 @@ class Priority(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
     deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()
+
+    class Meta:
+        unique_together = ("created_at", "name")
 
     def __str__(self):
         return self.name
