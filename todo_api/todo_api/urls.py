a7d34e094ab1b5fc16ea331a3afc2c9349a28070
Vladimir <153288910+velotes-com@users.noreply.github.com>
2025-08-14 17:26:17 +0300

    Update urls.py
    update

--- todo_api/todo_api/urls.py
+++ todo_api/todo_api/urls.py
@@ -17,10 +17,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ToDo API",
+        default_version='v1',
+        description="API documentation for To-Do service",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path("api/token/", obtain_auth_token, name="api_token_auth"),
     path("api/", include("api.urls")),
-]
+] 
